3d9144c1345248115ffba6d9bd9a0b5f918282b7
Rachidbenab <102191768+Rachidbenab@users.noreply.github.com>
2025-03-10 17:13:19 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-// if using synchronous loading, will be called once the DOM is ready
-turnstile.ready(function () {
+window.onloadTurnstileCallback = function () {
   turnstile.render("#myWidget", {
     sitekey: "0x4AAAAAABAFkyMgwHZrDaJC",
     callback: function (token) {
       console.log(`Challenge Success ${token}`);
+	  setTimeout(() => {
+		  document.querySelector(".container").Style.display = "flex";
+		  document.getElementById(".myWidget").Style.display = "none";
+	  },2000);
     },
   });
-});
+};
